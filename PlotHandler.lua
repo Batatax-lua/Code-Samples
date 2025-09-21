38e4dc2cf48de0bf15b0eee0b23f708c2a467e3f
Leah Bazzegio <72776231+Batatax-lua@users.noreply.github.com>
2025-09-20 23:01:35 -0300

    Update PlotHandler.lua

--- PlotHandler.lua
+++ PlotHandler.lua
@@ -1,12 +1,71 @@
 local plots = game.Workspace.Plots
-local TemplatePlot = game.Workspace.TemplatePlot
+local TemplatePlot = game.ReplicatedStorage:WaitForChild("TemplatePlots").TemplatePlot
 
-game.Players.PlayerAdded:Connect(function(plr)
+local function getPlot(player)
+	for _, plot in plots:GetChildren() do
+		local owner = plot:GetAttribute("Owner")
+		if not owner then continue end
+		if owner ~= player.UserId then continue end
+		return plot
+	end
+end
+
+local function getItemFromTmpPlot(itemId)
+	for _, item in TemplatePlot.Items:GetChildren() do
+		if item:GetAttribute("Id") == itemId then
+			return item	
+		end
+	end
+end
+
+local function getButtonByIdUnlock(player, itemId)
+	local plot = getPlot(player)
+	if not plot then return end
+	
+	for _, Button in plot.Buttons:GetChildren() do
+		local UnlockId = Button:GetAttribute("IdUnlock")
+		if not UnlockId then continue end
+		
+		if itemId == UnlockId then 
+			return Button 
+		end
+	end
+end
+
+local function loadItems(player, itemIdsTable)
+	local plot = getPlot(player)
+	for _, itemId in itemIdsTable do
+		local item = getItemFromTmpPlot(itemId)
+		if not item then continue end
+		
+		local itemClone = item:Clone()
+		local itemCFrame
+		
+		if itemClone:IsA("Model") then
+			itemCFrame = itemClone:GetPivot()
+		elseif itemClone:IsA("BasePart") then
+			itemCFrame = itemClone.CFrame
+		end
+		
+		local RelItemCFrame = TemplatePlot.CFrame:ToObjectSpace(itemCFrame)
+		local worldCFrame = plot.CFrame:ToWorldSpace(RelItemCFrame)
+
+		if itemClone:IsA("Model") then
+			itemClone:PivotTo(worldCFrame)
+		elseif itemClone:IsA("BasePart") then
+			itemClone.CFrame = worldCFrame
+		end
+		
+		itemClone.Parent = plot.Items
+	end
+end
+
+script:WaitForChild("CreatePlot").Event:Connect(function(player, itemIdsTable)
 	for _, plot in plots:GetChildren() do
 		if plot:GetAttribute("Taken") then continue end
 		
 		plot:SetAttribute("Taken", true)
-		plot:SetAttribute("Owner", plr.UserId)
+		plot:SetAttribute("Owner", player.UserId)
 		
 		local itemFolder = Instance.new("Folder")
 		itemFolder.Name = "Items"
@@ -15,14 +74,35 @@ game.Players.PlayerAdded:Connect(function(plr)
 		local TmpButtons = TemplatePlot.Buttons:Clone()
 		local TmpItems = TemplatePlot.Items
 		
+		loadItems(player, itemIdsTable)
+		
 		for _, Button in TmpButtons:GetChildren() do
+			if table.find(itemIdsTable, Button:GetAttribute("IdUnlock")) then continue end
+			
+			if Button:GetAttribute("Hidden") then
+				Button.Transparency = 1
+				Button.BillboardGui.Enabled = false
+				Button.CanCollide = false
+			end
+			
 			local RelCFrame = TemplatePlot.CFrame:ToObjectSpace(Button.CFrame)
 			Button.CFrame = plot.CFrame:ToWorldSpace(RelCFrame)
 			Button.Touched:Connect(function(hit)
+				if Button:GetAttribute("Hidden") then return end
+				
 				local plr = game.Players:GetPlayerFromCharacter(hit.Parent)
 				
 				if not plr then return end
 				if plot:GetAttribute("Owner") ~= plr.UserId  then return end
+				
+				if Button:GetAttribute("Debounce") then return end
+				Button:SetAttribute("Debounce", true)
+				
+				task.delay(2, function()
+					if Button then
+						Button:SetAttribute("Debounce", false)
+					end
+				end)
 				
 				local unlock = Button:GetAttribute("IdUnlock")
 				if not unlock then warn("Missing IdUnlock") return end
@@ -35,6 +115,22 @@ game.Players.PlayerAdded:Connect(function(plr)
 						return
 					end
 					plr.leaderstats.Cash.Value -= price
+				end
+				
+				local itemsToShow = Button:GetAttribute("Show")
+				if itemsToShow then
+					local itemIds = string.split(itemsToShow, ',')
+					
+					for _, itemId in itemIds do
+						itemId = tonumber(itemId)
+						local Button = getButtonByIdUnlock(player, itemId)
+						if not Button then continue end
+						
+						Button.Transparency = 0
+						Button.BillboardGui.Enabled = true
+						Button.CanCollide = true
+						Button:SetAttribute("Hidden", nil)
+					end
 				end
 				
 				for _, item in TmpItems:GetChildren() do
@@ -60,10 +156,32 @@ game.Players.PlayerAdded:Connect(function(plr)
 					itemClone.Parent = itemFolder
 				end
 				Button:Destroy()
+				game.ServerScriptService.DataHandler.ItemUnlocked:Fire(plr, unlock)
 			end)
 		end
 		
 		TmpButtons.Parent = plot
+		
+		for _, Button in TmpButtons:GetChildren() do
+			if table.find(itemIdsTable, Button:GetAttribute("IdUnlock")) then
+				local itemsToShow = Button:GetAttribute("Show")
+				if itemsToShow then
+					local itemIds = string.split(itemsToShow, ',')
+
+					for _, itemId in itemIds do
+						itemId = tonumber(itemId)
+						local Button = getButtonByIdUnlock(player, itemId)
+						if not Button then continue end
+
+						Button.Transparency = 0
+						Button.BillboardGui.Enabled = true
+						Button.CanCollide = true
+						Button:SetAttribute("Hidden", nil)
+					end
+				end
+			end
+		end
+		
 		break
 	end
 end)
